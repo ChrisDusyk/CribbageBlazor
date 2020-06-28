19ec60de6f6e4a7a0fcb58e81c58dfc1f1bcde98
Chris Dusyk <chris.dusyk@gmail.com>
2020-06-28 16:14:09 -0600

    Begin building the actual game session

---
+++ src/CribBlazor/Server/Hubs/PlayingRoundHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CribBlazor.Server.Hubs
+{
+	public class PlayingRoundHub : Hub<IPlayingRoundHub>
+	{
+		public async Task PlayCard(string playerId, string gameId, string card)
+		{
+			await Clients.Group(gameId).CardPlayed(playerId, card);
+		}
+	}
+}
