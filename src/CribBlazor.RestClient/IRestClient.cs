76546e87957d5eba47203d389ac6242cd967f309
Chris Dusyk <chris.dusyk@gmail.com>
2020-07-24 17:04:20 -0600

    Started writing functional rest client

---
+++ src/CribBlazor.RestClient/IRestClient.cs
@@ -0,0 +1,15 @@
+﻿using CribBlazor.Shared.Errors;
+using Functional;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CribBlazor.RestClient
+{
+	public interface IRestClient
+	{
+		Task<Result<TSuccess, ApplicationError>> GetFromJsonAsync<TSuccess>(string uri, CancellationToken cancellationToken);
+	}
+}
---
+++ src/CribBlazor.RestClient/RestClient.cs
@@ -0,0 +1,28 @@
+﻿using CribBlazor.Shared.Errors;
+using Functional;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using System.Net.Http;
+using System.Threading;
+using CribBlazor.Shared.Errors.ErrorCodes;
+
+namespace CribBlazor.RestClient
+{
+	internal class RestClient : IRestClient
+	{
+		private HttpClient Client { get; }
+
+		public RestClient(HttpClient httpClient)
+			=> Client = httpClient;
+
+		public Task<Result<TSuccess, ApplicationError>> GetFromJsonAsync<TSuccess>(string uri, CancellationToken cancellationToken)
+			=> Result.TryAsync(() =>
+			{
+				return Client.GetFromJsonAsync<TSuccess>(uri, cancellationToken);
+			})
+			.MapOnFailure(ex => RestError.Create(ex.Message, ex, ErrorCodes.RestCommunicationErrorCode.Create(uri)));
+	}
+}
