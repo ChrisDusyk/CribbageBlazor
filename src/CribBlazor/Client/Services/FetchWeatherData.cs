2320202b8302c2835d3aca300055572d17c852cb
Chris Dusyk <chris.dusyk@gmail.com>
2020-07-27 17:35:44 -0600

    Test using functional rest client with weather code from template

---
+++ src/CribBlazor/Client/Services/FetchWeatherData.cs
@@ -0,0 +1,10 @@
+﻿using CribBlazor.Shared;
+using CribBlazor.Shared.Errors;
+using Functional;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CribBlazor.Client.Services
+{
+	public delegate Task<Result<WeatherForecast[], ApplicationError>> FetchWeatherData(CancellationToken cancellationToken);
+}
---
+++ src/CribBlazor/Client/Services/Handlers/FetchWeatherDataHandler.cs
@@ -0,0 +1,22 @@
+﻿using CribBlazor.RestClient;
+using CribBlazor.Shared;
+using CribBlazor.Shared.Errors;
+using Functional;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CribBlazor.Client.Services.Handlers
+{
+	public class FetchWeatherDataHandler
+	{
+		private IRestClient RestClient { get; }
+
+		public FetchWeatherDataHandler(IRestClient restClient)
+			=> RestClient = restClient;
+
+		public Task<Result<WeatherForecast[], ApplicationError>> GetResult(CancellationToken cancellationToken)
+			=> from weatherData in RestClient.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast", cancellationToken)
+			   select weatherData;
+	}
+}
